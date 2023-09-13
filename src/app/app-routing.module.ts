48287c548b3ba4ffa95b12509e1ff72ccd1376bc
unknown <paledirefilwe@gmail.com>
2023-09-13 15:16:33 +0200

    transition to angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminDashComponent } from './admin-dash/admin-dash.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { RegisterComponent } from './register/register.component';
+import { SubjectComponent } from './subject/subject.component';
+import { TimetableComponent } from './timetable/timetable.component';
+import { UploadComponent } from './upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'dashboard',component:AdminDashComponent},
+  {path:'login',component:AdminLoginComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'subject',component:SubjectComponent},
+  {path:'timetable',component:TimetableComponent},
+  {path:'upload',component:UploadComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TimetableComponent } from './timetable/timetable.component';
+import { SubjectComponent } from './subject/subject.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { NavComponent } from './nav/nav.component';
+import { AdminDashComponent } from './admin-dash/admin-dash.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { RegisterComponent } from './register/register.component';
+import { UploadComponent } from './upload/upload.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TimetableComponent,
+    SubjectComponent,
+    GalleryComponent,
+    NavComponent,
+    AdminDashComponent,
+    AdminLoginComponent,
+    RegisterComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
